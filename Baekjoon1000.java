8edc24e04136d24e4042de9ec803fc3eb0ad1189
YellowH2312 <89592727+YellowH2312@users.noreply.github.com>
2021-10-06 19:57:57 +0900

    Update Baekjoon1000.java

--- Baekjoon1000.java
+++ Baekjoon1000.java
@@ -1,10 +1,12 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Baekjoon1000 {
     public static void main(String[] args) throws IOException {
-        int a = System.in.read()-'0';
-        System.in.read();
-        int b = System.in.read()-'0';
-        System.out.println(a+b);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        sc.close();
     }
 }
