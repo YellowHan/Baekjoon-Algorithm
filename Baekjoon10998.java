13c37915c6c1359d4c11c423af473a87eb549801
YellowH2312 <hellou8363@gmail.com>
2021-10-07 13:43:50 +0900

    A*B
    Baekjoon : 10998

---
+++ Baekjoon10998.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Baekjoon10998 {
+    public static void main(String[] args) throws IOException {
+        int a = System.in.read()-'0';
+        System.in.read();
+        int b = System.in.read()-'0';
+        System.out.println(a * b);
+    }
+}
